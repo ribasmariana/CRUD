2c92157f282877ac77073fc64dca29693652bb95
mariana.ribas <mariana.248.ribas@gmail.com>
2023-07-03 19:53:03 -0300

    Enums categoria e tipos de projeto

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,4 +1,20 @@
 //ISTO SERÁ UMA ENUM
 
-public class Categoria {
+public enum Categoria {
+    TECNOLOGIA("Tecnologia"),
+    ENGENHARIA("Engenharia"),
+    AGRONOMIA("Agronomia"),
+    SAUDE("Saúde"),
+    EDUCACAO("Educação"),
+    SOCIAL("Social");
+
+    private String descricaoCategoria;
+
+    Categoria (String descricaoCategoria) {
+        this.descricaoCategoria = descricaoCategoria;
+    }
+
+    public String getDescricaoCategoria() {
+        return descricaoCategoria;
+    }
 }
--- src/TipoProjeto.java
+++ src/TipoProjeto.java
@@ -1,4 +1,21 @@
 //ISTO SERÁ UMA ENUM
 
-public class TipoProjeto {
+public enum TipoProjeto {
+    PESQUISA("Pesquisa"),
+    EXTENSAO("Extensão"),
+    GRADUACAO("Graduação"),
+    POS_GRADUACAO("Pós graduação"),
+    MESTRADO("Mestrado"),
+    DOUTORADO("Doutorado"),
+    STARTUP("Startup");
+
+    private String descricaoTipo;
+
+    TipoProjeto(String descricaoTipo) {
+        this.descricaoTipo = descricaoTipo;
+    }
+
+    public String getDescricao() {
+        return descricaoTipo;
+    }
 }
