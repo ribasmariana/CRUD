533e6b65a6026ebbdea6ed21ede842edb4dbf914
mariana.ribas <mariana.248.ribas@gmail.com>
2023-07-03 19:58:47 -0300

    rede social e cidade

--- src/RedeSocial.java
+++ src/RedeSocial.java
@@ -1,2 +1,30 @@
 public class RedeSocial {
+
+    private Integer id;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public RedeSocial(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public RedeSocial(String descricao) {
+        this.descricao = descricao;
+    }
 }
