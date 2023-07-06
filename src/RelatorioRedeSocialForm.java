41184ccb9b86e03f110e6009ce0b964fb20e0f7c
maria.silvano <mariaeduardat.silvano@gmail.com>
2023-07-06 19:40:00 -0300

    Relatório da rede social

--- src/RelatorioRedeSocialForm.java
+++ src/RelatorioRedeSocialForm.java
@@ -1,2 +1,75 @@
-public class RelatorioRedeSocialForm {
+import Relatorios.TableRedeSocial;
+import model.Projeto;
+import Relatorios.TableProjeto;
+import model.RedeSocial;
+
+import javax.swing.*;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Vector;
+
+public class RelatorioRedeSocialForm extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String[] nomeColunas =
+            {"Código", "Descrição", "Código da Instituição", " "};
+
+    protected JTable table;
+    protected JScrollPane scroller;
+    protected TableRedeSocial tabela;
+
+    public RelatorioRedeSocialForm(Vector<RedeSocial> vetorDados) {
+        iniciarComponentes(vetorDados);
+    }
+
+    public void iniciarComponentes(Vector<RedeSocial> vetorDados) {
+        tabela = new TableRedeSocial(nomeColunas, vetorDados);
+        table = new JTable();
+        table.setModel(tabela);
+        table.setSurrendersFocusOnKeystroke(true);
+        scroller = new javax.swing.JScrollPane(table);
+        table.setPreferredScrollableViewportSize(new java.awt.Dimension(500, 300));
+
+        TableColumn colunaEscondida = table.getColumnModel().getColumn(TableRedeSocial.INDEX_ESCONDIDO);
+        colunaEscondida.setMinWidth(2);
+        colunaEscondida.setPreferredWidth(2);
+        colunaEscondida.setMaxWidth(2);
+        setLayout(new BorderLayout());
+        add(scroller, BorderLayout.CENTER);
+    }
+
+    public static void emitirRelatorio(List<RedeSocial> redeSocials) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            JFrame frame = new JFrame("Relatorio");
+
+            frame.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent evt) {
+                    frame.setVisible(false);
+                    try {
+                        Main.chamaMenuPrincipal();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Vector<RedeSocial> vetorDados = new Vector<RedeSocial>();
+            for (RedeSocial redeSocial : redeSocials) {
+                vetorDados.add(redeSocial);
+            }
+            frame.getContentPane().add(new RelatorioRedeSocialForm(vetorDados));
+            frame.pack();
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
